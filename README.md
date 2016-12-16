2aef826203214fb7e8aca2bc9a84b8216b0c4264
Jagadish Kumar <jagadishblr@gmail.com>
2016-12-16 18:27:20 +0530

    json data poputation

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This is a static implementation. Please download the code as zip file and unzip on your hard drive. Open the index.html to view application.
 
-Implementation nedded:
+Implementation needed:
   > Table for 5 day forecast need to be build.
   > Bootstrap to be included for UI.
--- js/scripts.js
+++ js/scripts.js
@@ -5,32 +5,15 @@ $(function () {
         type: 'GET',
         dataType: "json",
         url: 'http://api.openweathermap.org/data/2.5/forecast?q=bangalore&units=metric&cnt=5&appid=c11f28be057ef1c94c9c1b7ad8836c56',
-        success: function(data, textStatus, jqXHR) {
-            $weatherdata.append('<h3>City: '+ data.city.name +'</h3>');
-            $weatherdata.append('<p>Temperature: '+ data.list[0].main.temp +'</p>');
-            $weatherdata.append('<p>Min. Temperature: '+ data.list[0].main.temp_min +'</p>');
-            $weatherdata.append('<p>Max. Temperature: '+ data.list[0].main.temp_max +'</p>');
-            drawTable(data);
-        }
+        error: function() {
+           $('#weatherdata').html('<p>An error has occurred</p>');
+        },
+        dataType: 'jsonp',
+        success: function(data) {
+            $.each(data.list, function() {
+            var temp = "<tr><td>"+this.dt_txt+"</td><td>"+this.main.temp_min+"</td><td>"+this.main.temp_max+"</td></tr>";
+                $("#weatherDataTable").append(temp);
+                });
+        },
     });
 });
-
-function drawTable(data) {
-    var rows = [];
-
-    for (var i = 0; i < data.list.length; i++) {
-        console.log(data.list.length);
-        rows.push(drawRow(data.list[i]));
-    }
-
-    $("#weatherDataTable").append(rows);
-}
-
-function drawRow(rowData) {
-    var row = $("<tr />")
-    row.append($("<td>" + rowData.list[i].dt_txt + "</td>"));
-    row.append($("<td>" + rowData.list[i].main.temp_min + "</td>"));
-    row.append($("<td>" + rowData.list[i].main.temp_max + "</td>"));
-
-    return row;
-}
