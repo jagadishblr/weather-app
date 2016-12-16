acc96c48a6f54e56bb644568af864c1e2f7db175
Jagadish Kumar <jagadishblr@gmail.com>
2016-12-16 19:57:48 +0530

    Bootstrap integration

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,13 @@ $(function () {
     $.ajax({
         type: 'GET',
         dataType: "json",
-        url: 'http://api.openweathermap.org/data/2.5/forecast?q=bangalore&units=metric&cnt=5&appid=c11f28be057ef1c94c9c1b7ad8836c56',
+        url: 'http://api.openweathermap.org/data/2.5/forecast?q=bangalore&units=metric&appid=c11f28be057ef1c94c9c1b7ad8836c56',
         error: function() {
            $('#weatherdata').html('<p>An error has occurred</p>');
         },
         dataType: 'jsonp',
         success: function(data) {
+            $('h1 span').append(data.city.name);
             $.each(data.list, function() {
             var temp = "<tr><td>"+this.dt_txt+"</td><td>"+this.main.temp_min+"</td><td>"+this.main.temp_max+"</td></tr>";
                 $("#weatherDataTable").append(temp);
